c5b4a3c7dfa7629e877eac59b37ab8ef9ed6c5be
David Alkire <david.alkire@gmail.com>
2011-12-01 10:13:19 -0500

    Initial Commit

---
+++ LCV/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LCV
+//
+//  Created by David Alkire on 12/1/11.
+//  Copyright (c) 2011 Harvard Medical School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LCV/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LCV
+//
+//  Created by David Alkire on 12/1/11.
+//  Copyright (c) 2011 Harvard Medical School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
